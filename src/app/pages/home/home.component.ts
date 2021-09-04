2881c428cac2566365fc92b8fc9f11ecbec1563b
m emad <mrnoop22@gmail.com>
2021-09-04 19:44:48 +0200

    pagination in shop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,7 @@ export class HomeComponent implements OnInit {
   showNavigationArrows = false;
   showNavigationIndicators = false;
   images = [20, 5, 111].map((n) => `https://picsum.photos/id/${n}/1366/552`);
+  smallimages = [250, 15, 14].map((n) => `https://picsum.photos/id/${n}/500/270`);
   constructor() { }
 
   ngOnInit(): void {
